5c838ad068fb1f91ea92bcdc3b932769dc5dc087
YunByungKwan <pk7955@naver.com>
2022-08-14 15:26:29 +0900

    feat - 로그인화면에 임시 카카오 로그인 버튼 추가 2-feature

--- app/src/main/java/com/kwancorp/mbti/ui/login/LoginScreen.kt
+++ app/src/main/java/com/kwancorp/mbti/ui/login/LoginScreen.kt
@@ -1,12 +1,25 @@
 package com.kwancorp.mbti.ui.login
 
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kwancorp.mbti.ui.theme.MbtiTheme
 
 @Composable
 fun LoginScreen() {
     // TODO
+    Button(
+        onClick = { /*TODO*/ }
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterVertically),
+            text = "카카오 로그인"
+        )
+    }
 }
 
 @Preview
