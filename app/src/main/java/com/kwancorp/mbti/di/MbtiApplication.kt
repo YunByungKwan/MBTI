dba8fb5aa30238000e36d6c7b27b5fdee490eaca
YunByungKwan <pk7955@naver.com>
2022-08-14 15:18:45 +0900

    feat - 카카오SDK 환경셋팅 2-feature

--- app/src/main/java/com/kwancorp/mbti/di/MbtiApplication.kt
+++ app/src/main/java/com/kwancorp/mbti/di/MbtiApplication.kt
@@ -1,12 +1,17 @@
 package com.kwancorp.mbti.di
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.kwancorp.mbti.BuildConfig
 import org.koin.core.context.startKoin
 
 class MbtiApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
 
         startKoin {
             modules(appModule)
